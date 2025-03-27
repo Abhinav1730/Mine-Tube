d61b59cc5ea3012c498319674fa010174a07ed78
Abhinav1730 <abhinav.saxena1730@gmail.com>
2025-03-27 20:30:27 +0530

    routing

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateUserAvatar,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +34,10 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken)
+router.route('/change-password').post(verifyJWT,changeCurrentPassword)
+router.route('/current-user').get(verifyJWT,getCurrentUser)
+router.route('/update-avatar').patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route('/channel/:username').get(verifyJWT,getUserChannelProfile)
+router.route('/watch-history').get(verifyJWT,getWatchHistory)
 
 export default router;
